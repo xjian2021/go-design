129b755f758f1d85e5c1afdb3e4dc46fc208fedd
zpj <784093680@qq.com>
2022-11-20 20:56:31 +0800

    feat:新增剑指offer35题解 chore:补充剑指24翻转链表的递归解法以及总结

--- leetcode/offer-24-fan-zhuan-lian-biao-lcof/main.go
+++ leetcode/offer-24-fan-zhuan-lian-biao-lcof/main.go
@@ -14,7 +14,8 @@ func main() {
 	fmt.Println("输入：")
 	listNode.EchoListNode()
 	fmt.Println("输出：")
-	listNode = reverseList(listNode)
+	//listNode = reverseList(listNode)
+	listNode = reverseList2(listNode)
 	listNode.EchoListNode()
 }
 
@@ -35,4 +36,22 @@ func reverseList(head *pkg.ListNode) *pkg.ListNode {
 		tmp = next
 	}
 	return out
+}
+
+//递归法
+// 1->2->3->nil
+// 3->2->1->nil
+//f(node):逆转node为头节点的链表
+//f(node.next):逆转node.next为头节点的链表
+//f(node) = f(node.next) + (node.next.next=node + node.next=nil):逆转当前节点和把下下节点指向当前节点(逆转链表操作)
+// 递归写法的技巧在于把每一层与下一层的关系找出来，然后只需要处理好当前层需要做的操作以及下一层的递归调用即可
+// 还需要把终止条件找出来!!!
+func reverseList2(head *pkg.ListNode) *pkg.ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	newNode := reverseList2(head.Next)
+	head.Next.Next = head
+	head.Next = nil // 这里之所以没事，是因为在把2的next改为nil前已经把1的next改为2了，建立了联系之后，往上一层就是3的next是2，所以不会断点
+	return newNode
 }
---
+++ leetcode/offer-35-fu-za-lian-biao-de-fu-zhi-lcof/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	null = math.MinInt
+)
+
+//剑指 Offer 35. 复杂链表的复制
+//请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，每个节点除了有一个 next 指针指向下一个节点，还有一个 random 指针指向链表中的任意节点或者 null。
+
+func main() {
+	//[[3,null],[5,17],[4,null],[-9,6],[-10,3],[5,15],[0,11],[6,null],[-6,16],[3,16],[-6,11],[9,12],[-2,1],[-3,11],[-1,10],[2,11],[-3,null],[-9,7],[-2,4],[-8,null],[5,null]]
+	node := SliceToNode([]int{3, 5, 4, -9, -10, 5, 0, 6, -6, 3, -6, 9, -2, -3, -1, 2, -3, -9, -2, -8, 5}, []int{null, 17, null, 6, 3, 15, 11, null, 16, 16, 11, 12, 1, 11, 10, 11, null, 7, 4, null, null})
+	fmt.Println("输入：")
+	node.EchoNode()
+	fmt.Println("输出：")
+	newNode := copyRandomList(node)
+	newNode.EchoNode()
+}
+
+type Node struct {
+	Val    int
+	Next   *Node // 指向下一个节点
+	Random *Node // 指向任意节点或nil
+}
+
+func SliceToNode(s []int, r []int) *Node {
+	ln := &Node{}
+	tmp := ln
+	for _, i := range s {
+		// 带头节点
+		tmp.Next = &Node{
+			Val:  i,
+			Next: nil,
+		}
+		// 带尾节点
+		//tmp.Val = i
+		//tmp.Next = &ListNode{}
+		tmp = tmp.Next
+	}
+
+	for n, v := range r {
+		var index int
+		var nNode *Node
+		var iNode *Node
+		for i := ln.Next; i != nil; i = i.Next {
+			if index == n {
+				nNode = i
+			}
+			if index == v {
+				iNode = i
+			}
+			index++
+		}
+		if nNode != nil && iNode != nil {
+			nNode.Random = iNode
+		}
+	}
+
+	return ln.Next
+}
+
+func (t *Node) EchoNode() {
+	var index int
+	for i := t; i != nil; i = i.Next {
+		fmt.Printf("%d node[%[2]p]:%+[2]v\n", index, i)
+		index++
+	}
+}
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Next *Node
+ *     Random *Node
+ * }
+ */
+
+func copyRandomList(head *Node) *Node {
+	if head == nil {
+		return head
+	}
+	rondomSet := []int{}
+	nodeSet := []*Node{}
+	for i := head; i != nil; i = i.Next {
+		nodeSet = append(nodeSet, &Node{
+			Val:    i.Val,
+			Next:   nil,
+			Random: nil,
+		})
+		r := null
+		if i.Random != nil {
+			r = getNodeIndex(head, i.Random)
+		}
+		rondomSet = append(rondomSet, r)
+	}
+	MaxNode := len(nodeSet)
+	for i, node := range nodeSet {
+		if rondomSet[i] != null {
+			node.Random = nodeSet[rondomSet[i]]
+		}
+		if i+1 < MaxNode {
+			nodeSet[i].Next = nodeSet[i+1]
+		}
+	}
+	return nodeSet[0]
+}
+
+func getNodeIndex(head *Node, target *Node) int {
+	var index int
+	for i := head; i != nil; i = i.Next {
+		if i == target {
+			return index
+		}
+		index++
+	}
+	return null
+}
